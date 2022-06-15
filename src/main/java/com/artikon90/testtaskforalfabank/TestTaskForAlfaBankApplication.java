8ef078854816b952ff32c111f6aefb3c424fbf2c
artikon90 <artikon90@gmail.com>
2022-06-15 17:31:53 +0300

    Init commit

---
+++ src/main/java/com/artikon90/testtaskforalfabank/TestTaskForAlfaBankApplication.java
@@ -0,0 +1,13 @@
+package com.artikon90.testtaskforalfabank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestTaskForAlfaBankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestTaskForAlfaBankApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/artikon90/testtaskforalfabank/TestTaskForAlfaBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.artikon90.testtaskforalfabank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestTaskForAlfaBankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
