c4371663c3c9d4f4f87db88ff19ce5775232db09
artikon90 <artikon90@gmail.com>
2022-06-18 19:45:59 +0300

    Подключил feign

--- src/main/java/com/artikon90/testtaskforalfabank/TestTaskForAlfaBankApplication.java
+++ src/main/java/com/artikon90/testtaskforalfabank/TestTaskForAlfaBankApplication.java
@@ -2,8 +2,10 @@ package com.artikon90.testtaskforalfabank;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class TestTaskForAlfaBankApplication {
 
     public static void main(String[] args) {
