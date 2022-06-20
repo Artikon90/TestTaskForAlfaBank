b9ce2350249bccf0b5dd09b915bb5a24b3418bc2
artikon90 <artikon90@gmail.com>
2022-06-20 21:55:38 +0300

    Добавил билдеры на модели для упрощения создания

--- src/main/java/com/artikon90/testtaskforalfabank/model/Course.java
+++ src/main/java/com/artikon90/testtaskforalfabank/model/Course.java
@@ -1,5 +1,6 @@
 package com.artikon90.testtaskforalfabank.model;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,6 +8,7 @@ import java.util.Map;
 
 @Getter
 @Setter
+@Builder
 public class Course {
     private String disclaimer;
     private String license;
--- src/main/java/com/artikon90/testtaskforalfabank/model/Gif.java
+++ src/main/java/com/artikon90/testtaskforalfabank/model/Gif.java
@@ -1,11 +1,13 @@
 package com.artikon90.testtaskforalfabank.model;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 import java.util.Map;
 
 @Getter
 @Setter
+@Builder
 public class Gif {
     private Map<String, Object> data;
     private Map<String, String> meta;
